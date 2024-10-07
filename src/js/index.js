58ad761f81f058bc434bd53f6905c3b43ae6626f
KyungMin.Ko <rhrudals312@naver.com>
2024-10-07 16:35:02 +0900

    초기 세팅 완료

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import MainPage from "../components/MainPage.js";
-import SignInPage from "../components/SignInPage.js";
-import { router } from "./router.js";
-
-// 기본적으로 메인 페이지를 로드
-document.addEventListener("DOMContentLoaded", () => {
-  MainPage(); // 메인 페이지 호출
-});
